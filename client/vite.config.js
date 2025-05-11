22bbed581e49574a67f5a30e08a689639752d6a2
impcakash <impcakash@gmail.com>
2025-05-11 22:18:06 +0530

    code updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -14,6 +13,7 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
   },
+  // Only useful during local development
   server: {
     proxy: {
       "/api": {
@@ -22,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
